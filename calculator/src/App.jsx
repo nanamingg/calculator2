484581bacf492caf1e9a6b1999b3084280620235
nanamingg <nana172@kakao.com>
2023-11-09 15:34:38 +0900

    fin

--- calculator/src/App.jsx
+++ calculator/src/App.jsx
@@ -1,5 +1,77 @@
+import { useState } from "react";
+import CalInput from "./components/CalInput";
+import CalNumberBtn from "./components/CalNumberBtn";
+import CalDelBtn from "./components/CalDelBtn";
+import CalOperatorBtn from "./components/CalOperatorBtn";
+import CalEnterBtn from "./components/CalEnterBtn";
+
 const App = () => {
-  return <div className="bg-red-100">Hello, React!</div>;
+  const [result, setResult] = useState("0");
+  const [temp, setTemp] = useState();
+  const [operator, setOperator] = useState();
+
+  return (
+    <main className="bg-blue-100 min-h-screen flex justify-center items-center">
+      <div className="bg-gray-300 px-8 py-12 rounded-lg shadow-md">
+        <CalInput result={result} setResult={setResult} />
+        <div className="grid grid-cols-4 mt-8 gap-4">
+          <CalNumberBtn result={result} setResult={setResult} number="1" />
+          <CalNumberBtn result={result} setResult={setResult} number="2" />
+          <CalNumberBtn result={result} setResult={setResult} number="3" />
+          <CalOperatorBtn
+            result={result}
+            setResult={setResult}
+            setTemp={setTemp}
+            setOperator={setOperator}
+            inputOperator="+"
+            bgColor="bg-pink-500 active:bg-pink-700"
+          />
+          <CalNumberBtn result={result} setResult={setResult} number="4" />
+          <CalNumberBtn result={result} setResult={setResult} number="5" />
+          <CalNumberBtn result={result} setResult={setResult} number="6" />
+          <CalOperatorBtn
+            result={result}
+            setResult={setResult}
+            setTemp={setTemp}
+            setOperator={setOperator}
+            inputOperator="-"
+            bgColor="bg-yellow-400 active:bg-yellow-700"
+          />
+          <CalNumberBtn result={result} setResult={setResult} number="7" />
+          <CalNumberBtn result={result} setResult={setResult} number="8" />
+          <CalNumberBtn result={result} setResult={setResult} number="9" />
+          <CalOperatorBtn
+            result={result}
+            setResult={setResult}
+            setTemp={setTemp}
+            setOperator={setOperator}
+            inputOperator="*"
+            bgColor="bg-red-500 active:bg-red-700"
+          />
+          <button className="btn-style bg-green-500 active:bg-green-700">
+            <CalEnterBtn
+              result={result}
+              setResult={setResult}
+              temp={temp}
+              setTemp={setTemp}
+              operator={operator}
+              setOperator={setOperator}
+            />
+          </button>
+          <CalNumberBtn result={result} setResult={setResult} number="0" />
+          <CalDelBtn result={result} setResult={setResult} />
+          <CalOperatorBtn
+            result={result}
+            setResult={setResult}
+            setTemp={setTemp}
+            setOperator={setOperator}
+            inputOperator="/"
+            bgColor="bg-cyan-300 active:bg-cyan-900"
+          />
+        </div>
+      </div>
+    </main>
+  );
 };
 
 export default App;
